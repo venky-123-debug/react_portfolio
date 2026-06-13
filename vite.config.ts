3dfed400dd86de2689258692276279ad4a7b96e1
Venkatesh C <venkatbe26494@gmail.com>
2026-06-13 15:47:03 +0530

    sourcemap added to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "./",
-  server:{
-    host:"localhost",
-    port:3000,
-       
-  }
-})
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+
+  build: {
+    sourcemap: false,
+  },
+});
